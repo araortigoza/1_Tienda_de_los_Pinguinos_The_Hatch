7b71fecfb7a9ab56a222c10c0a5820e0642095b0
araortigoza <araprtogoza17@gmail.com>
2026-05-12 18:09:39 -0300

    Se creo coleccion de orders con MongoDB

---
+++ backend/models/orderModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phonenumber: {
+        type: Number,
+        required: true
+    },
+    products: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+            },
+            productname: {
+                type: String,
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            },
+            total: {
+                type: Number,
+                required: true
+            },
+        },
+    ],
+    orderTotal: {
+        type: Number,
+        required: true
+    },
+});
+
+module.exports = mongoose.model("Order", orderSchema);
