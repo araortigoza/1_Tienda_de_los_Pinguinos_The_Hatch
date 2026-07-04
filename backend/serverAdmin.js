ea9c91594a0754c8eef069efec384b0a760e1f20
araortigoza <araprtogoza17@gmail.com>
2026-07-04 11:39:32 -0300

    Se implemento configuraciones del servidor

--- backend/serverAdmin.js
+++ backend/serverAdmin.js
@@ -7,6 +7,20 @@ require("./middlewares/database")
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
+
+app.set("view engine", "pug");
+app.set("views", (path.join__dirname, 'views'));
+
+app.use("/login", rutas);
+app.use("/dashborard", rutas);
+
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
 app.listen(5000, () => {
     console.log("Servidor activo en http://localhost:5000");
 });
