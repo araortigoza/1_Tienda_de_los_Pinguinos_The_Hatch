9e12a0b997421ce68e09570e346815603e891ae9
araortigoza <araprtogoza17@gmail.com>
2026-07-08 18:01:03 -0300

    Se implementaron controladores

---
+++ frontend/controllers/storeController.js
@@ -0,0 +1,64 @@
+const Product = require("../models/productsModel");
+const Order = require("../models/orderModel");
+
+async function showStore(req, res) {
+    const products = await Product.find();
+    const puffles = products.filter(product => product.price === 0);
+    const food = products.filter(product => product.price > 0);
+
+    res.render("store", { puffles: puffles, food: food });
+}
+
+async function addToCart(req, res) {
+    const { id } = req.params;
+    const quantity = req.body.quantity ? Number(req.body.quantity) : 1;
+
+    const product = await Product.findById(id);
+
+    if (!req.session.cart) {
+        req.session.cart = [];
+    }
+
+    req.session.cart.push({
+        productId: product._id,
+        productname: product.productname,
+        quantity: quantity,
+        price: product.price,
+        total: product.price * quantity
+    });
+
+    res.redirect("/store");
+}
+
+async function showCart(req, res) {
+    const cart = req.session.cart || [];
+    const cartTotal = cart.reduce((sum, item) => sum + item.total, 0);
+
+    res.render("cart", { cart: cart, cartTotal: cartTotal });
+}
+
+async function checkout(req, res) {
+    const { name, address, phonenumber } = req.body;
+    const cart = req.session.cart || [];
+
+    if (cart.length === 0) {
+        return res.redirect("/store");
+    }
+
+    const orderTotal = cart.reduce((sum, item) => sum + item.total, 0);
+
+    const newOrder = new Order({
+        name: name,
+        address: address,
+        phonenumber: phonenumber,
+        products: cart,
+        orderTotal: orderTotal
+    });
+
+    await newOrder.save();
+    req.session.cart = [];
+
+    res.render("confirmation", { order: newOrder });
+}
+
+module.exports = { showStore, addToCart, showCart, checkout };
