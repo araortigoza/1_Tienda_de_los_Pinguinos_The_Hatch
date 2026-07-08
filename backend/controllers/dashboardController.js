3de5b7565304ee028568541e8a4b4c8ee630bfb1
araortigoza <araprtogoza17@gmail.com>
2026-07-08 02:55:22 -0300

    Implementacion de controladores

---
+++ backend/controllers/dashboardController.js
@@ -0,0 +1,63 @@
+const Product = require("../models/productsModel");
+const Order = require("../models/orderModel");
+
+async function showDashboard(req, res) {
+    const products = await Product.find();
+    const orders = await Order.find();
+    res.render("dashboard", { products: products, orders: orders });
+}
+
+async function createProduct(req, res) {
+    const { productname, price, stock } = req.body;
+    const image = req.file ? req.file.filename : "";
+
+    const newProduct = new Product({
+        productname: productname,
+        price: price,
+        stock: stock,
+        image: image
+    });
+
+    await newProduct.save();
+    res.redirect("/dashboard");
+}
+
+async function editProduct(req, res) {
+    const { id } = req.params;
+    const { productname, price, stock } = req.body;
+
+    const updateData = {
+        productname: productname,
+        price: price,
+        stock: stock
+    };
+
+    if (req.file) {
+        updateData.image = req.file.filename;
+    }
+
+    await Product.findByIdAndUpdate(id, updateData);
+    res.redirect("/dashboard");
+}
+
+async function deleteProduct(req, res) {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect("/dashboard");
+}
+
+async function updateOrderStatus(req, res) {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    await Order.findByIdAndUpdate(id, { status: status });
+    res.redirect("/dashboard");
+}
+
+module.exports = {
+    showDashboard,
+    createProduct,
+    editProduct,
+    deleteProduct,
+    updateOrderStatus
+};
