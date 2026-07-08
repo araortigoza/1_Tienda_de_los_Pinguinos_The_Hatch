f9ab7b8898cc65ddf4ec514f3a043471c7be4bef
araortigoza <araprtogoza17@gmail.com>
2026-07-08 15:46:32 -0300

    Se creo coleccion de products con MongoDB

---
+++ frontend/models/productsModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    productname: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    stock: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    image: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Product", productSchema);
