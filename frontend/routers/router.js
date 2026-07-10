850b2cc846b79accdb241ca834ac16fc701bfe9a
araortigoza <araprtogoza17@gmail.com>
2026-07-10 08:05:45 -0300

    Se agregaron comentarios

--- frontend/routers/router.js
+++ frontend/routers/router.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+// SE EXTRAE LO QUE SE NECESITA DE CONTROLLER
 const storeController = require("../controllers/storeController");
 
+// SE DEFINEN RUTAS
 router.get("/store", storeController.showStore);
 router.post("/cart/add/:id", storeController.addToCart);
 router.get("/cart", storeController.showCart);
 router.post("/checkout", storeController.checkout);
 
+// SE EXPORTAN LAS RUTAS
 module.exports = router;
