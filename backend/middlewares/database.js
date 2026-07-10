536940f2368a2e0cf30c3723a19d27d8f9b0929a
araortigoza <araprtogoza17@gmail.com>
2026-07-10 04:23:44 -0300

    Se agregaron comentarios

--- backend/middlewares/database.js
+++ backend/middlewares/database.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // SE IMPORTA MONGOOSE
 
+// SE INICIA LA CONEXION DE LA DB CON MONGO
 mongoose.connect("mongodb://127.0.0.1:27017/clubpenguin")
 .then(() =>{
     console.log("Base de datos conectada")
