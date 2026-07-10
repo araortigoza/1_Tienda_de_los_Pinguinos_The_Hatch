c27f906efebc97a95d881e9b079515b6b03a3851
araortigoza <araprtogoza17@gmail.com>
2026-07-10 07:31:47 -0300

    Se cambio a API RESTfull y se agregaron comentarios

--- backend/routers/router.js
+++ backend/routers/router.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const multer = require("multer");
-const path = require("path");
+const express = require("express"); // SE IMPORTA EXPRESS
+const router = express.Router(); // SE IMPORTA ROUTER PARA ENRUTAMIENTOS
+const multer = require("multer"); // SE IMPORTA MULTER PARA IMAGENES
+const path = require("path"); // SE IMPORTA PATH PARA DIRECCIONES DE ARCHIVOS
 
+// SE IMPORTA LO NECESARIO DE OTROS ARCHIVOS
 const loginController = require("../controllers/loginController");
 const dashboardController = require("../controllers/dashboardController");
 const checkAuth = require("../middlewares/authentication");
 
+// UTILIZANDO MULTER SE GUARDA LAS IMAGENES ENVIADAS EN LA CARPETA UPLOADS
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.join(__dirname, "..", "public", "uploads"));
@@ -16,16 +18,18 @@ const storage = multer.diskStorage({
     }
 });
 
+// SE ACTIVA MULTER PASANDO LOS DATOS GUARDADOS ANTERIORMENTE
 const upload = multer({ storage: storage });
 
+// SE DEFINEN RUTAS
 router.get("/login", loginController.showLogin);
 router.post("/login", loginController.login);
 router.get("/logout", loginController.logout);
 
 router.get("/dashboard", checkAuth, dashboardController.showDashboard);
-router.post("/dashboard/products/create", checkAuth, upload.single("image"), dashboardController.createProduct);
-router.post("/dashboard/products/edit/:id", checkAuth, upload.single("image"), dashboardController.editProduct);
-router.post("/dashboard/products/delete/:id", checkAuth, dashboardController.deleteProduct);
-router.post("/dashboard/orders/status/:id", checkAuth, dashboardController.updateOrderStatus);
+router.post("/dashboard/products", checkAuth, upload.single("image"), dashboardController.createProduct);
+router.put("/dashboard/products/:id", checkAuth, upload.single("image"), dashboardController.editProduct);
+router.delete("/dashboard/products/:id", checkAuth, dashboardController.deleteProduct);
+router.put("/dashboard/orders/:id", checkAuth, dashboardController.updateOrderStatus);
 
 module.exports = router;
