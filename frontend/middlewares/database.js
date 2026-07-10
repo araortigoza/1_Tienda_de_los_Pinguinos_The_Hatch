3d925a3b2fab92d73af1d6fa2bb53a7574e99a8d
araortigoza <araprtogoza17@gmail.com>
2026-07-10 08:04:28 -0300

    Se agregaron comentarios

--- frontend/middlewares/database.js
+++ frontend/middlewares/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// SE CONECTA LA BASE DE DATOS
 mongoose.connect("mongodb://127.0.0.1:27017/clubpenguin")
 .then(() =>{
     console.log("Base de datos conectada")
